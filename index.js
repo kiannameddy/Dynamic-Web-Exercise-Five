cd7d5d343cd593626cb080fb24578758fc94b50b
kiannaeddy <kme9504@nyu.edu>
2023-11-09 09:44:30 -0500

    added files

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const indexRoute = require('./routes/index');
-// const singlePostroute = require('./routes/singlePost');
+const singlePostRoute = require('./routes/singlePost');
 // const createPostRoute = require('./routes/CreatePost');
 
 app.use("/", indexRoute);
+app.use("/post", singlePostRoute);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
