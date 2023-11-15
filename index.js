db4b7993da0c3d3f68ce0c4fb91b5bc10f38a2f3
kiannaeddy <kme9504@nyu.edu>
2023-11-15 10:10:12 -0500

    added files

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express'); 
 const app = express();
+const router = express.Router();
 const firebase = require("firebase/app");
-//Port
 const port = 4000;
-//My Unqiue porject configuration information for firebase
+
 const firebaseConfig = {
     apiKey: "AIzaSyDrceUnHKwShVIWlrSANa6-pXCsP9HYPM4",
     authDomain: "exercise-five-fall-2023.firebaseapp.com",
@@ -13,15 +13,15 @@ const firebaseConfig = {
     appId: "1:176234642082:web:078d6a0776e320c5fe35fb"
   };
 
-//Initialize firbase
 firebase.initializeApp(firebaseConfig);
 
 const indexRoute = require('./routes/index');
 const singlePostRoute = require('./routes/singlePost');
-// const createPostRoute = require('./routes/CreatePost');
+const createPostRoute = require('./routes/CreatePost');
 
 app.use("/", indexRoute);
 app.use("/post", singlePostRoute);
+app.use("/create", createPostRoute);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
